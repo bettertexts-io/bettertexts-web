e945ca453eb2a0ee083229f7537b5df04dff9b09
Dominik <dominik.scherm@reaction.link>
2023-01-27 22:55:36 +0000

    Add plausible.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import PlausibleProvider from "next-plausible";
 import "./globals.css";
 
 export default function RootLayout({
@@ -11,7 +12,9 @@ export default function RootLayout({
       style={{ scrollBehavior: "smooth" }}
       className="h-full scroll-smooth bg-white antialiased [font-feature-settings:'ss01']"
     >
-      <head />
+      <head>
+        <PlausibleProvider domain="bettertexts.io" />
+      </head>
       <body>{children}</body>
     </html>
   );
